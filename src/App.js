120118579a60766344076b17e8cf314cf4fa737d
Jacob Roe <jroenavy24@gmail.com>
2021-07-12 15:44:28 -0700

    changes include MORE FORMS assignment

--- src/App.js
+++ src/App.js
@@ -2,20 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 import UserFormLive from "./components/UserFormLive";
 
-// Creating functional component
 function App() {
-  const [state, setState] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: ""
-  });
   
-  // Show UserFormLive.jsk 
   return (
     <div className="App">
-      <UserFormLive inputs={state} setInputs={setState} />
+      <UserFormLive/>
     </div>
   );
 }
--- src/components/UserFormLive.jsx
+++ src/components/UserFormLive.jsx
@@ -1,41 +1,58 @@
 import React, { useState } from 'react';
 
+// Functional Component w/ hook (useState)
 const UserFormLive = (props) => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+    const [state, setState] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
 
+    // Event Handler for change
+    const handleChange = e => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        });
+        }
+
+    // Added validations to all form fields
     return (
         <form>
             <div>
                 <label>First Name: </label>
-                <input type="text" onChange={(e) => setFirstName(e.target.value)} />
+                <input onChange={handleChange} type="text" name="firstName" />
+                { (state.firstName.length !== 0 && state.firstName.length < 2) && <p>First Name must be at least 2 characters.</p> }
             </div>
             <div>
                 <label>Last Name: </label>
-                <input type="text" onChange={(e) => setLastName(e.target.value)} />
+                <input onChange={handleChange} type="text" name="lastName" />
+                { (state.lastName.length !== 0 && state.lastName.length < 2) && <p>Last Name must be at least 2 characters.</p> }
             </div>
             <div>
                 <label>Email: </label>
-                <input type="text" onChange={(e) => setEmail(e.target.value)} />
+                <input onChange={handleChange} type="text" name="email" />
+                { (state.email.length !== 0 && state.email.length < 5) && <p>Email must be at least 5 characters.</p> }
             </div>
             <div>
                 <label>Password: </label>
-                <input type="text" onChange={(e) => setPassword(e.target.value)} />
+                <input onChange={handleChange} type="password" name="password" />
+                { (state.password.length !== 0 && state.password.length < 8) && <p>Password must be at least 8 characters.</p> }
             </div>
             <div>
                 <label>Confirm Password: </label>
-                <input type="text" onChange={(e) => setConfirmPassword(e.target.value)} />
+                <input onChange={handleChange} type="password" name="confirmPassword" />
+                { (state.confirmPassword !== state.password) && <p>Passwords must match.</p> }
             </div>
-            <div>
+            {/* <div>
                 <p>First Name: {firstName}</p>
                 <p>Last Name: {lastName}</p>
                 <p>Email: {email}</p>
                 <p>Password: {password}</p>
                 <p>Confirm Password: {confirmPassword}</p>
-            </div>
+            </div> */}
         </form>
     );
 };
